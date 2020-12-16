681014e0130ecb4a3b3c2a4233627257ed5bc72d
LucasStarlingdePaulaSalles <lucastarling1@gmail.com>
2020-12-16 14:36:34 -0300

    Dev: Improving usability an code modularity

--- frontend/cmd/create-task.go
+++ frontend/cmd/create-task.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -18,61 +19,24 @@ import (
 func CreateTask() {
 	fmt.Println("Creating a task...\n")
 	task := models.Task{}
-	fmt.Print("Task name: ")
-	var err error
-	var text string
-	//Title input
-	text, err = lineReader()
-	if err != nil {
-		fmt.Printf("Erro: %s \n", err)
-		return
-	}
-	textParse(&task.Title, text)
-	//Description input
-	fmt.Print("Task description (up to 255 char): ")
-	text, err = lineReader()
-	if err != nil {
-		fmt.Printf("Erro: %s \n", err)
-		return
-	}
-	textParse(&task.Description, text)
-	//Priority input
-	var num int
-	fmt.Print("Task priority [1 : 10]: ")
-	_, err = fmt.Scan(&num)
-	if err != nil {
-		fmt.Printf("Erro: %s \n", err)
-		return
-	}
-	priorityParse(&task, num)
-	//Deadline input
-	fmt.Print("Task deadline [dd/mm/yyyy]: ")
-	fmt.Scan(&text)
-	err = dateParse(&task, text)
-	if err != nil {
-		fmt.Printf("Erro: %s \n", err)
-		return
-	}
-	//TimeEstimate input
-	fmt.Print("Task time estimate (h): ")
-	_, err = fmt.Scan(&num)
-	if err != nil {
-		fmt.Printf("Erro: %s \n", err)
-		return
-	}
-	estimateParse(&task, num)
-	//Dependencies input
+
+	getTitle(&task)
+	getDescription(&task)
+	getPriority(&task)
+	getDeadline(&task)
+	getTimeEstimate(&task)
 	getDependencies(&task)
+
 	fmt.Println()
 	fmt.Println("Your task:")
 	PrintTasks(task)
+
 	fmt.Println("Create? (yes/no)")
-	fmt.Scan(&text)
-	fmt.Println(text)
-	if text == "yes" || text == "y" {
+	var input string
+	fmt.Scan(&input)
+	fmt.Println(input)
+	if input == "yes" || input == "y" {
 		sendTask(task)
-	} else {
-		return
 	}
 }
 
@@ -105,39 +69,82 @@ func sendTask(task models.Task) {
 	}
 }
 
-func lineReader() (string, error) {
-	in := bufio.NewReader(os.Stdin)
-	line, err := in.ReadString('\n')
-	line = line[:len(line)-1]
-	return line, err
-}
-
-func textParse(field *string, value string) {
-	if len(value) > 256 {
-		*field = value[0:255]
-	} else {
-		*field = value
+func getTitle(task *models.Task) {
+	for {
+		fmt.Print("Task name: ")
+		text, err := lineReader()
+		if err == nil {
+			if text != "" {
+				textParse(&task.Title, text)
+				return
+			}
+			err = errors.New("Title is required")
+		}
+		fmt.Printf("Erro: %s \n\n", err)
 	}
 }
 
-func dateParse(task *models.Task, dateStr string) error {
+func getDescription(task *models.Task) {
+	for {
+		fmt.Print("Task description (up to 255 char): ")
+		text, err := lineReader()
+		if err == nil {
+			textParse(&task.Description, text)
+			return
+		}
+		fmt.Printf("Erro: %s \n", err)
+	}
+}
+
+func getPriority(task *models.Task) {
+	var priority int
+	for {
+		fmt.Print("Task priority [1 : 10]: ")
+		_, err := fmt.Scan(&priority)
+		if err == nil {
+			if priority > 0 && priority < 11 {
+				task.Priority = uint8(priority)
+				return
+			}
+			err = errors.New("Priority must be an integer between 1 and 10")
+		}
+		fmt.Printf("Erro: %s \n", err)
+	}
+}
+
+func getDeadline(task *models.Task) {
 	layout := "02/01/2006"
 	var err error
-	task.Deadline, err = time.Parse(layout, dateStr)
-	return err
-}
-
-func priorityParse(task *models.Task, priority int) {
-	if priority < 1 {
-		priority = 1
-	} else if priority > 10 {
-		priority = 10
+	var input string
+	for {
+		fmt.Print("Task deadline [dd/mm/yyyy]: ")
+		fmt.Scan(&input)
+		task.Deadline, err = time.Parse(layout, input)
+		if err == nil {
+			return
+		}
+		err = errors.New("Connot parse '" + input + "' as 'dd/mm/yyyy'")
+		fmt.Printf("Erro: %s \n", err)
 	}
-	task.Priority = uint8(priority)
 }
 
-func estimateParse(task *models.Task, duration int) {
-	task.TimeEstimate = time.Duration(duration) * time.Hour
+func getTimeEstimate(task *models.Task) {
+	var input string
+	var err error
+	var hours float64
+	for {
+		fmt.Print("Task time estimate (h): ")
+		_, err = fmt.Scan(&input)
+		if err == nil {
+			hours, err = strconv.ParseFloat(input, 64)
+			fmt.Println(err)
+			if err == nil {
+				task.TimeEstimate = time.Duration(hours*60) * time.Minute
+				return
+			}
+		}
+		fmt.Printf("Erro: %s \n", err)
+	}
 }
 
 func getDependencies(task *models.Task) {
@@ -165,5 +172,20 @@ while:
 		} else {
 			break while
 		}
+	}
+}
+
+func lineReader() (string, error) {
+	in := bufio.NewReader(os.Stdin)
+	line, err := in.ReadString('\n')
+	line = line[:len(line)-1]
+	return line, err
+}
+
+func textParse(field *string, value string) {
+	if len(value) > 256 {
+		*field = value[0:255]
+	} else {
+		*field = value
 	}
 }
--- frontend/cmd/print-task.go
+++ frontend/cmd/print-task.go
@@ -13,11 +13,11 @@ func PrintTasks(tasks ...models.Task) {
 	const padding = 3
 
 	w := tabwriter.NewWriter(os.Stdout, 12, 0, padding, ' ', tabwriter.Debug)
-	fmt.Fprintf(w, "ID\t Name\t Status\t Priority\t Deadline\t Work done\t Dependencies\t Description\n")
+	fmt.Fprintf(w, "ID\t Name\t Status\t Priority\t Deadline\t TimeEstimate\t Work done\t Dependencies\t Description\n")
 	for _, task := range tasks {
-		fmt.Fprintf(w, "%d\t %s\t %s\t %d\t %s\t %.1f\t %s\t %s\n",
+		fmt.Fprintf(w, "%d\t %s\t %s\t %d\t %s\t %.1f\t %.1f\t %s\t %s\n",
 			task.ID, task.Title, parseStastus(task.Status), task.Priority, task.Deadline.Format("02/01/2006"),
-			task.WorkedFor.Hours(), task.Dependencies, task.Description)
+			task.TimeEstimate.Hours(), task.WorkedFor.Hours(), task.Dependencies, task.Description)
 	}
 	w.Flush()
 }
