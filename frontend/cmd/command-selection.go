11930b9fd1a3313f19cdad42829fd42f5aed97c2
LucasStarlingdePaulaSalles <lucastarling1@gmail.com>
2020-12-16 15:08:47 -0300

    Dev : Configured programm for receiving commands as execution arguments

--- frontend/cmd/command-selection.go
+++ frontend/cmd/command-selection.go
@@ -4,12 +4,54 @@ import (
 	"fmt"
 )
 
+//Init starts the program on interactive mode, on this mode the user can execute multiple commands.
+//Interactive mode is only interrupted by the user.
 func Init() {
-	Help()
-	Selector()
+	help()
+	var input string
+while:
+	for {
+		fmt.Print("->")
+		fmt.Scan(&input)
+		fmt.Println()
+		fmt.Println()
+		exit := Selector(input)
+		if exit {
+			break while
+		}
+	}
 }
 
-func Help() {
+//Selector executes a function based on its command
+func Selector(arg string) bool {
+	switch arg {
+	case "new":
+		CreateTask()
+	case "help":
+		help()
+	case "all":
+		GetAllTasks()
+	case "work":
+		Work()
+	case "stop":
+		StopWork()
+	case "status":
+		ChangeStatus()
+	case "close":
+		CloseTask()
+	case "tasks":
+		GetFilteredTasks()
+	case "exit":
+		fmt.Println("Goodbye!")
+		return true
+	default:
+		fmt.Println("Unknown command :(")
+		fmt.Println("Use 'help' for details")
+	}
+	return false
+}
+
+func help() {
 	fmt.Println("Command options:")
 	fmt.Println("help     |                Show this information")
 	fmt.Println("exit     |                    Close application")
@@ -18,41 +60,7 @@ func Help() {
 	fmt.Println("work     |              Start working on a task")
 	fmt.Println("stop     |   Stop work on currently active task")
 	fmt.Println("status   |               Change a task's status")
-	fmt.Println("close   |                          Close a task")
-	fmt.Println("tasks   |        Show tasks that match a filter")
+	fmt.Println("close    |                         Close a task")
+	fmt.Println("tasks    |       Show tasks that match a filter")
 	fmt.Println()
-}
-
-func Selector() {
-	var input string
-while:
-	for {
-		fmt.Print("->")
-		fmt.Scan(&input)
-		fmt.Println()
-		fmt.Println()
-
-		switch input {
-		case "new":
-			CreateTask()
-		case "help":
-			Help()
-		case "all":
-			GetAllTasks()
-		case "work":
-			Work()
-		case "stop":
-			StopWork()
-		case "status":
-			ChangeStatus()
-		case "close":
-			CloseTask()
-		case "tasks":
-			GetFilteredTasks()
-		case "exit":
-			break while
-
-		}
-	}
-
 }
--- frontend/main.go
+++ frontend/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/LucasStarlingdePaulaSalles/insprTasks/frontend/cmd"
 )
 
 func main() {
+	args := os.Args
+	if len(args) == 2 {
+		cmd.Selector(args[1])
+	} else if len(args) == 1 {
+		fmt.Println("Welcome to InsprTasks!")
+		cmd.Init()
+	} else {
+		fmt.Println("Error: Incorrect argument structure")
+	}
 
-	fmt.Println("Welcome to InsprTasks!")
-	cmd.Init()
 }
